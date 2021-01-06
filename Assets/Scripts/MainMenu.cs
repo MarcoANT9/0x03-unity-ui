a7f50596589cce1da0db2fd75fe0d6a3c8993c7c
MarcoAnt9 <acalde27@hotmail.com>
2021-01-06 17:53:18 -0500

    Task 11: First Commit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -9,7 +9,10 @@ public class MainMenu : MonoBehaviour
     // Public Variables ==============================================================
     //================================================================================
     public Button playButton;
+    public Button optionsButton;
+    public Button backButton;
     public Button quitButton;
+    public GameObject optionMenu;
 
     //================================================================================
     // Private Variables =============================================================
@@ -22,6 +25,8 @@ public class MainMenu : MonoBehaviour
     {
         playButton.onClick.AddListener(PlayMaze);
         quitButton.onClick.AddListener(QuitMaze);
+        optionsButton.onClick.AddListener(OptionsMenu);
+        backButton.onClick.AddListener(BackToMain);
     }
 
     //================================================================================
@@ -40,11 +45,27 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("maze");
     }
+    // Ends the game...
     public void QuitMaze()
     {
         Application.Quit();
         Debug.Log("Quit Game");
     }
-
+    // Takes out the options
+    public void OptionsMenu()
+    {
+        playButton.gameObject.SetActive(false);
+        optionsButton.gameObject.SetActive(false);
+        quitButton.gameObject.SetActive(false);
+        optionMenu.SetActive(true);
+    }
+    // Go back to main
+    public void BackToMain()
+    {
+        playButton.gameObject.SetActive(true);
+        optionsButton.gameObject.SetActive(true);
+        quitButton.gameObject.SetActive(true);
+        optionMenu.SetActive(false);
+    }
 
 }
